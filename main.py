0aaea561d6e21ea4795265a7ffd53b075d17de81
rinat <swatlprus@gmail.com>
2022-01-10 17:06:06 +0500

    Hello Tony Stark

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello Rinat')
-    print('Hello Pitter Parker')
+    print('Hello Tony Stark')
+    
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
